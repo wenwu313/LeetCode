f27fb78e77dab57ded652570e2f88477914d3864
taowenwu <724683889@qq.com>
2018-02-09 16:30:56 +0800

    update

--- leetcode/110_BalancedTree.cpp
+++ leetcode/110_BalancedTree.cpp
@@ -1,46 +1,43 @@
-#include<iostream>
-#include<algorithm>
-using namespace std;
+class Solution1 {
+public:
+	bool IsBalanced(TreeNode* pRoot, int &depth){
+		if (!pRoot){
+			depth = -1;
+			return true;
+		}
+		int left, right;
+		if (IsBalanced(pRoot->left, left) && IsBalanced(pRoot->right, right)) {
+			int diff = left - right;
+			if (diff <= 1 && diff >= -1){
+				depth = 1 + (left>right ? left : right);
+				return true;
+			}
+		}
+		return false;
+	}
 
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	bool isBalanced(TreeNode* root) {
+		int depth;
+		return IsBalanced(root, depth);
+	}
 };
 
-int depth(TreeNode* root)
-{
-	if (root == NULL) return 0;
-	return max(depth(root->left), depth(root->right)) + 1;
-}
+class Solution2 {
+public:
+    bool isBalanced(TreeNode *root) {
+        int height=0;
+        return isBalancedUtil(root, height);
+    }
 
-bool isBalanced(TreeNode* root) {
-	if (!root) return true;
-	if (abs(depth(root->left) - depth(root->right)) > 1)
-		return false;
-	return isBalanced(root->left) && isBalanced(root->right);
-}
-
-TreeNode* creatBTree(int data[], int index, int n)
-{
-	TreeNode *pNode = NULL;
-	if (index < n && data[index]!=-1)  //-1令树结点为NULL
-	{
-		pNode = new TreeNode(data[index]);
-		pNode->left = creatBTree(data, 2 * index + 1, n);  //将二叉树按照层序遍历, 依次标序号, 从0开始
-		pNode->right = creatBTree(data, 2 * index + 2, n);
-	}
-	return pNode;
-}
-
-
-int main()
-{
-	int a[] = { 2, 3, 3, 4, 5, 5, -1, 8 };  
-	int size = sizeof(a) / sizeof(int);
-	TreeNode* root = creatBTree(a, 0, size);
-	cout << isBalanced(root) << endl;
-	cin.get();
-	return 0;
-}
+    bool isBalancedUtil(TreeNode* root, int& height){
+       if(root==NULL){
+          height=0;
+          return true;
+       }
+       int lh=0, rh=0;
+       bool isLeft = isBalancedUtil(root->left, lh);
+       bool isRight = isBalancedUtil(root->right, rh);
+       height = (lh > rh ? lh : rh) + 1;
+       return (abs(lh-rh)<=1 && isLeft && isRight);
+    }
+};
