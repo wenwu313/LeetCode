099415b3a38ff34e58b36a052f4fee8fd2292221
WenwuTao <724683889@qq.com>
2018-02-17 13:56:22 +0800

    update

--- leetcode/221_MaximalSquare.cpp
+++ leetcode/221_MaximalSquare.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
 	int maximalSquare(vector<vector<char>>& matrix) {
-		if (matrix.empty())
-			return 0;
-		int rows = matrix.size(), cols = matrix[0].size();
+		int rows = matrix.size();
+		if (rows <= 0) return 0;
+		int cols = matrix[0].size();
 		vector<vector<int>> len(rows, vector<int>(cols, 0));
 		int maxlen = 0;
-		for (int i = 0; i < rows; i++){
+		for (int i = 0; i<rows; i++) {
 			len[i][0] = matrix[i][0] - '0';
-			if (len[i][0] == 1)
-				maxlen = 1;
+			maxlen = max(len[i][0], maxlen);
 		}
-		for (int i = 0; i < cols; i++){
+		for (int i = 0; i<cols; i++) {
 			len[0][i] = matrix[0][i] - '0';
-			if (len[0][i] == 1)
-				maxlen = 1;
+			maxlen = max(len[0][i], maxlen);
 		}
-		for (int i = 1; i < rows; i++){
-			for (int j = 1; j < cols; j++){
-				if (matrix[i][j] == '0')
-					len[i][j] = 0;
-				else{
+		for (int i = 1; i<rows; i++) {
+			for (int j = 0; j<cols; j++) {
+				if (matrix[i][j] == '1') {
 					len[i][j] = min(min(len[i - 1][j], len[i][j - 1]), len[i - 1][j - 1]) + 1;
-					if (len[i][j]>maxlen)
-						maxlen = len[i][j];
+					maxlen = max(len[i][j], maxlen);
 				}
 			}
 		}
--- leetcode/268_MissNumber.cpp
+++ leetcode/268_MissNumber.cpp
@@ -1,46 +1,38 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int missingNumber(vector<int>& nums) {
-	int res = nums.size();  
-	for (int i = 0; i < nums.size(); i++){
-		res ^= i;
-		res ^= nums[i];
+class Solution {
+public:
+	int missingNumber(vector<int>& nums) {
+		int res = nums.size();
+		for (int i = 0; i < nums.size(); i++) {
+			res ^= i;
+			res ^= nums[i];
+		}
+		return res;
 	}
-	return res;
-}
 
-int main()
-{
-	vector<int> nums = { 0, 1, 3, 4, 5, 6, 7 };
-	cout << missingNumber(nums) << endl;
-	cin.get();
-	return 0;
-}
+	int missingNumber1(vector<int>& nums) {  //二分
+		int n = nums.size();
+		sort(nums.begin(), nums.end());
+		int left = 0, right = n, mid = 0;
+		while (left<right) {
+			mid = (left + right) >> 1;
+			if (nums[mid]>mid)
+				right = mid;
+			else
+				left = mid + 1;
+		}
+		return left;
+	}
 
-
-// int missingNumber(vector<int>& nums) { //二分 
-//     int n = nums.size();  
-//     sort(nums.begin(), nums.end());  
-//     int left = 0, right = n, mid = 0;  
-//     while (left<right){  
-//         mid = (left + right) >> 1;  
-//         if (nums[mid]>mid)  
-//             right = mid;  
-//         else  
-//             left = mid + 1;  
-//     }  
-//     return left;  
-// }  
-
-// int missingNumber(vector<int>& nums) {   //交换，用索引标注
-//     int n = nums.size();  
-//     for (int i = 0; i<n; i++){  
-//         while (nums[i]>0 && nums[i] <= n && nums[i] != i + 1)  
-//             swap(nums[nums[i] - 1], nums[i]);  
-//     }  
-//     for (int i = 0; i<n; i++)  
-//     if (nums[i] != i + 1) return i + 1;  
-//     return 0;  
-// } 
+	int missingNumber2(vector<int>& nums) {  //变换，用索引标注
+		int n = nums.size();
+		for (int i = 0; i<n; i++) {
+			while (nums[i] >= 0 && nums[i]<n && nums[i] != i)
+				swap(nums[i], nums[nums[i]]);
+		}
+		for (int i = 0; i<n; i++) {
+			if (i != nums[i])
+				return i;
+		}
+		return n;
+	}
+};
