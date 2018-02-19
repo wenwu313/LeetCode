6d599f06c63017bef6038d6b9ec3c008532e5763
WenwuTao <724683889@qq.com>
2018-02-19 13:16:48 +0800

    update

--- leetcode/542_01Matrix.cpp
+++ leetcode/542_01Matrix.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {  
-    if (matrix.empty())  
-        return matrix;  
-    int len1 = matrix.size(), len2 = matrix[0].size();  
-    vector<vector<int>> res(len1, vector<int>(len2, 0));  
-    typedef pair<int, int> tp;  
-    queue<tp> que;  
-    for (int i = 0; i < len1; i++){  
-        for (int j = 0; j < len2; j++){  
-            if (matrix[i][j] == 0)  
-                que.push(tp(i, j));  
-            else{  
-                res[i][j] = -1;  
-            }  
-        }  
-  
-    }  
-    vector<tp> dirs = { tp(-1, 0), tp(1, 0), tp(0, -1), tp(0, 1) };  
-    while (!que.empty()){  
-        tp point = que.front();  
-        que.pop();  
-        int x = point.first, y = point.second;  
-        for (int i = 0; i < dirs.size(); i++){             
-            int nx = x + dirs[i].first, ny = y + dirs[i].second;  
-            if (nx >= 0 && nx <len1 && ny >= 0 && ny < len2 && res[nx][ny] == -1){  
-                res[nx][ny] = res[x][y] + 1;  
-                que.push(tp(nx, ny));  
-            }  
-        }  
-    }  
-    return res;  
-}
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        if (matrix.empty())
+            return matrix;
+        queue<pair<int, int>> que;
+        int rows = matrix.size(), cols = matrix[0].size();
+        vector<vector<int>> res(rows, vector<int>(cols, 0));
+        vector<vector<int>> dirs = { { -1,0 },{ 1,0 },{ 0,-1 },{ 0,1 } };
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0)
+                    que.push({ i,j });
+                else
+                    res[i][j] = -1;
+            }
+        }
+
+        while (!que.empty()) {
+            pair<int, int> point = que.front();
+            int x = point.first, y = point.second;
+            que.pop();
+            for (int i = 0; i < dirs.size(); i++) {
+                int nx = x + dirs[i][0], ny = y + dirs[i][1];
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && res[nx][ny] == -1) {
+                    res[nx][ny] = res[x][y] + 1;
+                    que.push({ nx,ny });
+                }
+            }
+        }
+        return res;
+    }
 };
