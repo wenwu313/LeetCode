75c06a4246388f90e3033794c56b83439287ff4e
WenwuTao <724683889@qq.com>
2018-02-21 12:14:26 +0800

    update

--- leetcode/105_ConstructBinaryTreeFromPreorderInorderTraversal.cpp
+++ leetcode/105_ConstructBinaryTreeFromPreorderInorderTraversal.cpp
@@ -1,50 +1,21 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-using namespace std;
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, int ps, int pe, int is, int ie)
-{
-	if (ps > pe) return NULL;
-	TreeNode *root = new TreeNode(preorder[ps]);
-	int x;
-	for (int i = is; i <= ie; i++){
-		if (inorder[i] == root->val){
-			x = i;
-			break;
+class Solution {
+public:
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, int ps, int pe, int is, int ie){
+		if (ps > pe) return NULL;
+		TreeNode *root = new TreeNode(preorder[ps]);
+		int x;
+		for (int i = is; i <= ie; i++) {
+			if (inorder[i] == root->val) {
+				x = i;
+				break;
+			}
 		}
+		root->left = buildTree(preorder, inorder, ps + 1, ps + x - is, is, x - 1);
+		root->right = buildTree(preorder, inorder, pe - ie + x + 1, pe, x + 1, ie);
+		return root;
 	}
-	root->left = buildTree(preorder, inorder, ps + 1, ps + x - is, is, x - 1);
-	root->right = buildTree(preorder, inorder, pe - ie + x + 1, pe, x + 1, ie);
-	return root;
-}
 
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-	return buildTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
-}
-
-void Print(TreeNode* p)  //Ç°Ðò±éÀú
-{
-	if (NULL == p)
-		return;
-	cout << p->val << endl;
-	Print(p->left);
-	Print(p->right);
-}
-
-int main()
-{
-	vector<int> preorder = { 1, 3, 5, 2, 4, 6 };
-	vector<int> inorder = { 5, 3, 1, 4, 2, 6 };
-	TreeNode* root = buildTree(preorder, inorder);
-	Print(root);
-	cin.get();
-	return 0;
-}
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		return buildTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+	}
+};
